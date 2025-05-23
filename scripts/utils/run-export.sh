3feb637ddb151bac1756a9ef4c2da0a2b75141c3
Maximillian Heth <info@edfx.co>
2025-05-22 20:09:55 -0500

    Add script to set up Python virtual environment and export site URLs to Google Sheets

---
+++ scripts/utils/run-export.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. .env
+
+rm -rf venv
+
+set -a
+
+./python-venv.py create -n venv
+./python-venv.py activate venv
+. venv/bin/activate
+./python-venv.py install -r requirements.txt
+
+chmod +x ./export-site-urls-to-google-sheets.py
+
+./export-site-urls-to-google-sheets.py --creds-file=$CREDS_FILE --spreadsheet-id=1z20_GunM3s0WuAFSp2s0G7l3oSeLYcThpzhaZvQFgfc --check-json-key
+
+set +a
